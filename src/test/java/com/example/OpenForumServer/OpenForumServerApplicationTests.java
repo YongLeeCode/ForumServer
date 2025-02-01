478b4094c7226fbbef30845c87c556e554c9c153
YongLeeCode <yonghwan311@gmail.com>
2025-02-01 23:33:14 +0900

    first commit

---
+++ src/test/java/com/example/OpenForumServer/OpenForumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenForumServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenForumServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
