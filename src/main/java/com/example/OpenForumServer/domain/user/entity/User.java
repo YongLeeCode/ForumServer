59dfeaa7aa7571505f9d1fc087773f48413294a4
YongLeeCode <yonghwan311@gmail.com>
2025-02-02 23:35:41 +0900

    feat: 유저 엔티티 구현

--- src/main/java/com/example/OpenForumServer/domain/user/entity/User.java
+++ src/main/java/com/example/OpenForumServer/domain/user/entity/User.java
@@ -2,16 +2,25 @@ package com.example.OpenForumServer.domain.user.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
+@Getter
 public class User {
 
     @Id
@@ -21,11 +30,20 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @Setter
     @Column(nullable = false)
     private String name;
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @Builder
     public User(String email, String name, String password) {
