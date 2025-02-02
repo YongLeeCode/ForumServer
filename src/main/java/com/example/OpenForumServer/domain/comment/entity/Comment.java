edc18d43cced4ddbf57c9bf57b5068a391abc366
YongLeeCode <yonghwan311@gmail.com>
2025-02-02 10:35:04 +0900

    feat: 댓글 엔티티 구현

---
+++ src/main/java/com/example/OpenForumServer/domain/comment/entity/Comment.java
@@ -0,0 +1,60 @@
+package com.example.OpenForumServer.domain.comment.entity;
+
+import com.example.OpenForumServer.domain.forum.entity.Forum;
+import com.example.OpenForumServer.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "comments")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "forum_id", nullable = false)
+    private Forum forum;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Comment(Forum forum, String content, User user) {
+        this.forum = forum;
+        this.content = content;
+        this.user = user;
+    }
+}
