e1abed9e75db7b7c68cff294dce63df5f8f82ea3
YongLeeCode <yonghwan311@gmail.com>
2025-02-02 10:35:25 +0900

    feat: 좋아요 엔티티 구현

---
+++ src/main/java/com/example/OpenForumServer/domain/like/entity/Like.java
@@ -0,0 +1,44 @@
+package com.example.OpenForumServer.domain.like.entity;
+
+import com.example.OpenForumServer.domain.forum.entity.Forum;
+import com.example.OpenForumServer.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "likes")
+@Getter
+@NoArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "forum_id", nullable = false)
+    private Forum forum;
+
+
+    @Builder
+    public Like(User user, Forum forum) {
+        this.user = user;
+        this.forum = forum;
+    }
+}
