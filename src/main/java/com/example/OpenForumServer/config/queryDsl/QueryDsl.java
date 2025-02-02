20bbfed9998f01538807d4ae673228101108f4e5
YongLeeCode <yonghwan311@gmail.com>
2025-02-02 23:35:04 +0900

    feat: queryDsl config 파일 구현

--- src/main/java/com/example/OpenForumServer/config/queryDsl/QueryDsl.java
+++ src/main/java/com/example/OpenForumServer/config/queryDsl/QueryDsl.java
@@ -1,4 +1,19 @@
 package com.example.OpenForumServer.config.queryDsl;
 
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class QueryDsl {
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
