d43208411356f592d8af46a5ce3bc547e8c9bb19
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 18:39:35 +0900

    refactor: 데이터를 dto로 가져오도록 변경

--- src/main/java/com/example/OpenForumServer/domain/user/service/UserService.java
+++ src/main/java/com/example/OpenForumServer/domain/user/service/UserService.java
@@ -1,7 +1,15 @@
 package com.example.OpenForumServer.domain.user.service;
 
+import com.example.OpenForumServer.controller.user.request.UpdateUserRequest;
+import com.example.OpenForumServer.controller.user.response.GetUserResponseItem;
 import com.example.OpenForumServer.domain.user.dto.UserDto;
 
 public interface UserService {
     void createUser(UserDto userDto);
+
+    UserDto getUserById(Long id);
+
+    String updateUser(Long id, UserDto req);
+
+    String deleteUser(Long id);
 }
--- src/main/java/com/example/OpenForumServer/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/OpenForumServer/domain/user/service/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.example.OpenForumServer.domain.user.service;
 
+import com.example.OpenForumServer.controller.user.request.UpdateUserRequest;
+import com.example.OpenForumServer.controller.user.response.GetUserResponseItem;
 import com.example.OpenForumServer.domain.user.dto.UserDto;
+import com.example.OpenForumServer.domain.user.entity.User;
 import com.example.OpenForumServer.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,14 +12,33 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Transactional
     public void createUser(UserDto dto) {
         String encodedPw = bCryptPasswordEncoder.encode(dto.getPassword());
         userRepository.save(dto.toEntity(encodedPw));
+    }
+
+    public UserDto getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not found user id"));
+        return UserDto.fromEntity(user);
+    }
+
+    public String updateUser(Long id, UserDto dto) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found user id"));
+        user.setName(dto.getName());
+        userRepository.save(user);
+        return "변경 성공?";
+    }
+
+    public String deleteUser(Long id) {
+        userRepository.deleteById(id);
+        return "삭제 성공?";
     }
 }
