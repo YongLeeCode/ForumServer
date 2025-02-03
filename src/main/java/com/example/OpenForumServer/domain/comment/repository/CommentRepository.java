2432810554aade11a3689f1401736c69b838354b
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 21:59:37 +0900

    feat: 댓글 레포지토리 구현

--- src/main/java/com/example/OpenForumServer/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/OpenForumServer/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,10 @@
 package com.example.OpenForumServer.domain.comment.repository;
 
 import com.example.OpenForumServer.domain.comment.entity.Comment;
+import com.example.OpenForumServer.domain.comment.repository.custom.CommentRepositoryCustom;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
+    List<Comment> findAllByForumId(Long forumId);
 }
