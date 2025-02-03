6b18f5d1576f21ef6a87bc80332cb1fc48202e95
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 22:00:34 +0900

    feat: 댓글 레포지토리 커스텀 구현

--- src/main/java/com/example/OpenForumServer/domain/comment/repository/custom/CommentRepositoryCustom.java
+++ src/main/java/com/example/OpenForumServer/domain/comment/repository/custom/CommentRepositoryCustom.java
@@ -1,4 +1,8 @@
 package com.example.OpenForumServer.domain.comment.repository.custom;
 
+import com.example.OpenForumServer.domain.comment.entity.Comment;
+import java.util.List;
+
 public interface CommentRepositoryCustom {
+    List<Comment> findAllByForumId(Long forumId);
 }
--- src/main/java/com/example/OpenForumServer/domain/comment/repository/custom/CommentRepositoryImpl.java
+++ src/main/java/com/example/OpenForumServer/domain/comment/repository/custom/CommentRepositoryImpl.java
@@ -1,4 +1,22 @@
 package com.example.OpenForumServer.domain.comment.repository.custom;
 
-public class CommentRepositoryImpl {
+import com.example.OpenForumServer.domain.comment.entity.Comment;
+import com.example.OpenForumServer.domain.comment.entity.QComment;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CommentRepositoryImpl implements CommentRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QComment qComment = QComment.comment;
+
+    @Override
+    public List<Comment> findAllByForumId(Long forumId) {
+        return jpaQueryFactory
+                .selectFrom(qComment)
+                .where(qComment.forum.id.eq(forumId))
+                .fetch();
+    }
 }
