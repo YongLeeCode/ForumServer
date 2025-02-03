760243ecbeb1f9a5648799a30cf5227873d4d483
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 21:58:37 +0900

    feat: 댓글 dto 구현

--- src/main/java/com/example/OpenForumServer/domain/comment/dto/CommentDto.java
+++ src/main/java/com/example/OpenForumServer/domain/comment/dto/CommentDto.java
@@ -1,16 +1,37 @@
 package com.example.OpenForumServer.domain.comment.dto;
 
+import com.example.OpenForumServer.controller.forum.request.CommentCreatorRequest;
+import com.example.OpenForumServer.controller.forum.response.GetCommentResponseItem;
 import com.example.OpenForumServer.domain.comment.entity.Comment;
 import com.example.OpenForumServer.domain.forum.entity.Forum;
 import com.example.OpenForumServer.domain.user.entity.User;
-import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class CommentDto {
+
     private final String content;
+    private final LocalDateTime updatedAt;
+    private final String userName;
+
+    public static CommentDto fromRequest(CommentCreatorRequest req) {
+        return new CommentDto(
+                req.getContent(),
+                req.getUpdatedAt(),
+                req.getUserName()
+        );
+    }
+
+    public GetCommentResponseItem toResponse() {
+        return GetCommentResponseItem.builder()
+                .content(this.content)
+                .updatedAt(this.updatedAt)
+                .userName(this.userName)
+                .build();
+    }
 
     public Comment toEntity(Forum forum, User user) {
         return Comment.builder()
@@ -18,5 +39,13 @@ public class CommentDto {
                 .forum(forum)
                 .user(user)
                 .build();
+    }
+
+    public static CommentDto fromEntity(Comment comment) {
+        return new CommentDto(
+                comment.getContent(),
+                comment.getUpdatedAt(),
+                comment.getUser().getName()
+        );
     }
 }
