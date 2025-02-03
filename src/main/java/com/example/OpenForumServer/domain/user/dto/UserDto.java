9836aa8b1754e09eafe15f6b3666831947cdbb36
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 18:38:13 +0900

    feat: req를 dto로 변환위한 메서드 구현

--- src/main/java/com/example/OpenForumServer/domain/user/dto/UserDto.java
+++ src/main/java/com/example/OpenForumServer/domain/user/dto/UserDto.java
@@ -1,16 +1,32 @@
 package com.example.OpenForumServer.domain.user.dto;
 
+import com.example.OpenForumServer.controller.user.request.UpdateUserRequest;
 import com.example.OpenForumServer.domain.user.entity.User;
+import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RequiredArgsConstructor
 @Getter
+@Builder
+@Setter
 public class UserDto {
-    private final String email;
     private final String name;
+    private final String email;
     private final String password;
+    private final LocalDateTime createdAt;
+
+    public static UserDto fromRequest(UpdateUserRequest req) {
+        return new UserDto(
+                req.getName(),
+                req.getEmail(),
+                req.getPassword(),
+                req.getCreatedAt()
+        );
+    }
 
     public User toEntity(String encodedPassword) {
         return User.builder()
@@ -18,5 +34,14 @@ public class UserDto {
                 .email(this.email)
                 .password(encodedPassword)
                 .build();
+    }
+
+    public static UserDto fromEntity(User user) {
+        return new UserDto(
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getCreatedAt()
+        );
     }
 }
