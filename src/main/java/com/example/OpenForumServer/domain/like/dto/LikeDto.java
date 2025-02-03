71c2e0c699cc293899561ef9cafc40c7d233ec22
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 18:33:06 +0900

    feat: req를 dto로 바꾸는 메서드 구현

--- src/main/java/com/example/OpenForumServer/domain/like/dto/LikeDto.java
+++ src/main/java/com/example/OpenForumServer/domain/like/dto/LikeDto.java
@@ -1,5 +1,6 @@
 package com.example.OpenForumServer.domain.like.dto;
 
+import com.example.OpenForumServer.controller.like.request.LikeRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,4 +9,8 @@ import lombok.RequiredArgsConstructor;
 public class LikeDto {
     private final Long user;
     private final Long forum;
+
+    public static LikeDto fromReq (LikeRequest req) {
+        return new LikeDto(req.getUser(), req.getForum());
+    }
 }
