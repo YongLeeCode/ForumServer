75245ce1e5adc0f0f6a38e4a7874a8cea08326b2
YongLeeCode <yonghwan311@gmail.com>
2025-02-03 18:34:43 +0900

    refactor: 유저 req, dto로 변환을 위한 수정

--- src/main/java/com/example/OpenForumServer/controller/user/request/UpdateUserRequest.java
+++ src/main/java/com/example/OpenForumServer/controller/user/request/UpdateUserRequest.java
@@ -1,4 +1,12 @@
 package com.example.OpenForumServer.controller.user.request;
 
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
 public class UpdateUserRequest {
+    private String name;
+    private final String email = "";
+    private final String password = "";
+    private final LocalDateTime createdAt = null;
 }
