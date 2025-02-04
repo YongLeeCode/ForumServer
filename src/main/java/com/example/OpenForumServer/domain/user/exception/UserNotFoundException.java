bc61e397853cfaef90ad1f3ee784d4bd433b2599
YongLeeCode <yonghwan311@gmail.com>
2025-02-04 23:27:49 +0900

    feat: 유저 예외 및 에러 파일 생성

---
+++ src/main/java/com/example/OpenForumServer/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.OpenForumServer.domain.user.exception;
+
+public class UserNotFoundException extends UserException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
