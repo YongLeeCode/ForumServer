5a05b6185feb87a42955dcec4fd3625061e89428
YongLeeCode <yonghwan311@gmail.com>
2025-02-04 23:28:41 +0900

    feat: 포럼 페이지 생성

--- src/main/java/com/example/OpenForumServer/domain/forum/repository/ForumRepository.java
+++ src/main/java/com/example/OpenForumServer/domain/forum/repository/ForumRepository.java
@@ -1,7 +1,9 @@
 package com.example.OpenForumServer.domain.forum.repository;
 import com.example.OpenForumServer.domain.forum.entity.Forum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ForumRepository extends JpaRepository<Forum, Long> {
-
+    Page<Forum> findAll(Pageable pageable);
 }
