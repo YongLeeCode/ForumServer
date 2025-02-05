ca486ec3978b5ec1ed879fa0eebb1577b3f3e452
YongLeeCode <yonghwan311@gmail.com>
2025-02-05 17:24:10 +0900

    refactor: 유저 req 생성자 일부 변경

--- src/main/java/com/example/OpenForumServer/controller/user/request/UserRequest.java
+++ src/main/java/com/example/OpenForumServer/controller/user/request/UserRequest.java
@@ -2,11 +2,18 @@ package com.example.OpenForumServer.controller.user.request;
 
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-public class UpdateUserRequest {
+public class UserRequest {
     private String name;
-    private final String email = "";
-    private final String password = "";
+    private String email;
+    private String password = "";
     private final LocalDateTime createdAt = null;
+
+    public UserRequest(String name, String email, String password){
+        this.name = name;
+        this.email = (email != null) ? email : "";
+        this.password = (password != null) ? password : "";
+    }
 }
